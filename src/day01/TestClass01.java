c93946de1858423e430a8c10ec92d65c12013c49
lee@email <leehak@email>
2023-05-19 19:37:01 +0900

    5-19

--- src/day01/TestClass01.java
+++ src/day01/TestClass01.java
@@ -1,14 +1,18 @@
 package day01;
-
+import java.util.Scanner;
 public class TestClass01 {
 
  public static void main(String []args) {
-	 
+	 Scanner sc = new Scanner(System.in);
+	 int age;
+	String name;
+	System.out.println("나이입력:");
+	age=sc.nextInt();
+	System.out.println("이름입력:");
+	name=sc.next();
+	System.out.println("당신의 나이는"+age+"이군요");
+	System.out.println("당신의 이름은_"+name+"이군요");
+	
  }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//집에서 작성
-	}
-
-
+}
